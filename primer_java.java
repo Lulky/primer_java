12289d85c81ad05e515e6cd422df44cf302ac045
Lulky <lukyluxiano.12@gmail.com>
2022-03-28 13:10:36 -0500

    primer java

---
+++ primer_java.java
@@ -0,0 +1,8 @@
+public class primer_java { 
+    public static void main(String[] args){
+
+        System.out.println("Mi nombre es Luciano Usnayo");
+        System.out.println("Tengo 29 años de edad");
+        System.out.println("Mi ciudad natal es Lima, Perú");
+    }
+}
